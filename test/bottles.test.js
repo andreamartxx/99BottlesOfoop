19f80d7d4b9f1d6c4169cb0c1e7c6ea516040e94
Andrea Martin <amartin.onda@gmail.com>
2022-04-07 09:21:26 +0200

    Pass test for the case if 'verses 2, 0, 1'

--- test/bottles.test.js
+++ test/bottles.test.js
@@ -21,36 +21,6 @@ describe('Bottles', () => {
     expect(bottles.verse(3)).toBe(expected);
   });
 
-  test('verse 2', () => {
-    const bottles = new Bottles();
-    const expected =
-      '2 bottles of beer on the wall, ' +
-      '2 bottles of beer.\n' +
-      'Take one down and pass it around, ' +
-      '1 bottle of beer on the wall.\n';
-    expect(bottles.verse(2)).toBe(expected);
-  });
-
-  test('verse 1', () => {
-    const bottles = new Bottles();
-    const expected =
-      '1 bottle of beer on the wall, ' +
-      '1 bottle of beer.\n' +
-      'Take it down and pass it around, ' +
-      'no more bottles of beer on the wall.\n';
-    expect(bottles.verse(1)).toBe(expected);
-  });
-
-  test('verse 0', () => {
-    const bottles = new Bottles();
-    const expected =
-      'No more bottles of beer on the wall, ' +
-      'no more bottles of beer.\n' +
-      'Go to the store and buy some more, ' +
-      '99 bottles of beer on the wall.\n';
-    expect(bottles.verse(0)).toBe(expected);
-  });
-
   test('a couple of verses', () => {
     const bottles = new Bottles();
     const expected =
