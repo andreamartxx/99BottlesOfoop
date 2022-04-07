8df1c384c4e5017ade0c0bc5ebc2ec9a5150470d
Andrea Martin <amartin.onda@gmail.com>
2022-04-07 10:25:28 +0200

    started againg with test verse 2

--- test/bottles.test.js
+++ test/bottles.test.js
@@ -21,39 +21,14 @@ describe('Bottles', () => {
     expect(bottles.verse(3)).toBe(expected);
   });
 
-  test('a couple of verses', () => {
-    const bottles = new Bottles();
-    const expected =
-      '99 bottles of beer on the wall, ' +
-      '99 bottles of beer.\n' +
-      'Take one down and pass it around, ' +
-      '98 bottles of beer on the wall.\n' +
-      '\n' +
-      '98 bottles of beer on the wall, ' +
-      '98 bottles of beer.\n' +
-      'Take one down and pass it around, ' +
-      '97 bottles of beer on the wall.\n';
-    expect(bottles.verse(98, 99)).toBe(expected);
-  });
-
-  test('a few verses', () => {
+  test('verse 2', () => {
     const bottles = new Bottles();
     const expected =
       '2 bottles of beer on the wall, ' +
       '2 bottles of beer.\n' +
       'Take one down and pass it around, ' +
-      '1 bottle of beer on the wall.\n' +
-      '\n' +
-      '1 bottle of beer on the wall, ' +
-      '1 bottle of beer.\n' +
-      'Take it down and pass it around, ' +
-      'no more bottles of beer on the wall.\n' +
-      '\n' +
-      'No more bottles of beer on the wall, ' +
-      'no more bottles of beer.\n' +
-      'Go to the store and buy some more, ' +
-      '99 bottles of beer on the wall.\n';
-    expect(bottles.verse(2, 0)).toBe(expected);
+      '1 bottle of beer on the wall.\n';
+    expect(bottles.verse(2)).toBe(expected);
   });
 
   test('the whole song', ()=>{
