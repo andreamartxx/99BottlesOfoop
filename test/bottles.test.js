824c1ffc49df7b8a8cb691c3ddb38e300b4293a4
Andrea Martin <amartin.onda@gmail.com>
2022-04-07 08:34:09 +0200

    Failing test for the case of 'verse 3'

--- test/bottles.test.js
+++ test/bottles.test.js
@@ -10,4 +10,14 @@ describe('Bottles', () => {
       '98 bottles of beer on the wall.\n';
     expect(bottles.verse(99)).toBe(expected);
   });
+
+  test('another verse', () => {
+    const bottles = new Bottles();
+    const expected =
+      '3 bottles of beer on the wall, ' +
+      '3 bottles of beer.\n' +
+      'Take one down and pass it around, ' +
+      '2 bottles of beer on the wall.\n';
+    expect(bottles.verse(3)).toBe(expected);
+  });
 });
