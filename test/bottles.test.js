8316c723a0fd953212a663bdc148a4ab020ede65
Andrea Martin <amartin.onda@gmail.com>
2022-04-07 10:37:51 +0200

    failing test for the case of verses 2, 1, 0

--- test/bottles.test.js
+++ test/bottles.test.js
@@ -66,6 +66,25 @@ describe('Bottles', () => {
     expect(bottles.verses(99, 98)).toBe(expected);
   });
 
+  test('a few verses', () => {
+     const expected =
+      '2 bottles of beer on the wall, ' +
+      '2 bottles of beer.\n' +
+      'Take one down and pass it around, ' +
+      '1 bottle of beer on the wall.\n' +
+      '\n' +
+      '1 bottle of beer on the wall, ' +
+      '1 bottle of beer.\n' +
+      'Take it down and pass it around, ' +
+      'no more bottles of beer on the wall.\n' +
+      '\n' +
+      'No more bottles of beer on the wall, ' +
+      'no more bottles of beer.\n' +
+      'Go to the store and buy some more, ' +
+      '99 bottles of beer on the wall.\n';
+    expect(bottles.verses(2, 0)).toBe(expected);
+  });
+
   test('the whole song', ()=>{
     const bottles = new Bottles();
     expect(bottles.song()).toBe(bottles.verses(99, 0));
