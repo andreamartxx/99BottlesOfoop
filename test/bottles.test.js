076af72f5b085c269f40b16a5b9f58b7259894ef
Andrea Martin <amartin.onda@gmail.com>
2022-04-07 08:41:13 +0200

    Failing test for the case of 'verse 2'

--- test/bottles.test.js
+++ test/bottles.test.js
@@ -20,4 +20,14 @@ describe('Bottles', () => {
       '2 bottles of beer on the wall.\n';
     expect(bottles.verse(3)).toBe(expected);
   });
+
+  test('verse 2', () => {
+    const bottles = new Bottles();
+    const expected =
+      '2 bottles of beer on the wall, ' +
+      '2 bottles of beer.\n' +
+      'Take one down and pass it around, ' +
+      '1 bottle of beer on the wall.\n';
+    expect(bottles.verse(2)).toBe(expected);
+  });
 });
